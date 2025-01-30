245d9e88d3381358a7500893817d864d40946628
solo1161996 <148924807+solo1161996@users.noreply.github.com>
2025-01-30 05:32:21 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const connect = require("./connect")
+const express = require("express")
+const cors = require("cors")
+const posts = require("./postRoutes")
+const users = require("./userRoutes")
+
+
+const app = express()
+const PORT = 3000
+
+app.use(cors())
+app.use(express.json())
+app.use(posts)
+app.use(users)
+
+app.listen(PORT, () => {
+    connect.connectToserver()
+    console.log(`Adrian! the server is running on port ${PORT}`)
+})
